bfbcb99cc2ed784652e0be351194b78a3025a5c2
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2020-01-12 09:35:19 -0300

    fazendo modificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
-function Header({ cartSize }) {
-  console.log('cart', cartSize)
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -22,8 +23,4 @@ function Header({ cartSize }) {
       </Cart>
     </Container>
   );
-}
-
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(Header);
+};
